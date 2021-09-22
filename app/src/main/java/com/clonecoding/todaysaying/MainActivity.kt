c72b5cc8fefb84582645aaf4d461db3ef2e0eade
Onegold <ujini1129@gmail.com>
2021-09-22 18:56:00 +0900

    [feature] 기본 UI 추가

--- app/src/main/java/com/clonecoding/todaysaying/MainActivity.kt
+++ app/src/main/java/com/clonecoding/todaysaying/MainActivity.kt
@@ -3,6 +3,10 @@ package com.clonecoding.todaysaying
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Remote config
+ * ViewPager2
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
