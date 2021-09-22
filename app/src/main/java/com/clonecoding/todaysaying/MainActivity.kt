18573336384b954fa20acc4adde05759fe80b8ba
Onegold <ujini1129@gmail.com>
2021-09-22 19:16:19 +0900

    [feature] ViewPager2 어댑터 추가

--- app/src/main/java/com/clonecoding/todaysaying/MainActivity.kt
+++ app/src/main/java/com/clonecoding/todaysaying/MainActivity.kt
@@ -2,14 +2,32 @@ package com.clonecoding.todaysaying
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 /**
  * Remote config
  * ViewPager2
  */
 class MainActivity : AppCompatActivity() {
+
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    /**
+     * 뷰를 초기화 합니다.
+     */
+    private fun initViews() {
+
+        this.viewPager.adapter = QuotesPagerAdapter(listOf(
+            Quote("테스트 명언", "테스트 명언 대상")
+        ))
     }
 }
